e67d5fcea69fe1bfd5f871fb0b59468dbbf8052c
mtri-git <vmtri20@gmail.com>
2025-09-26 18:43:12 +0700

    update style

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,19 @@
 // @ts-check
 
+// @ts-ignore
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+// @ts-ignore
 import { defineConfig } from 'astro/config';
+
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  site: 'https://example.com',
+  integrations: [mdx(), sitemap()],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
